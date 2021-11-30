dbac72281aae35ae671cf2ed37a32d38fca2017f
akshata2005 <akshatasawant1805@gmail.com>
2021-11-30 20:47:25 +0530

    my first commit

---
+++ coinflip.sh
@@ -0,0 +1,7 @@
+a=$((RANDOM%2))
+if [ $a -eq 1 ]
+then
+echo "Heads"
+else
+echo "Tails"
+fi
---
+++ final review/Helloworld.java
@@ -0,0 +1,17 @@
+public class Helloworld { public static void main(String argc[]) {
+         while(true)
+         {
+            System.out.println("Hello world to terminal");
+try
+{
+
+            Thread.sleep(6000);
+}
+catch(Exception ex)
+         {
+          System.out.println(ex);
+         }
+
+         }
+      }
+}
